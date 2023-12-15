6c1622da4c990bd42d6da229145b262436cb3f5e
Arix581 <espenben000@alpinesd.org>
2023-12-14 18:56:29 -0700

    Began Sea Turtle Boss Work

---
+++ Assets/Scripts/EnemyScripts/BossScripts/TurtleAttacks.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleAttacks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Turtle Pope attack (do nothing) (morallity attack?) (dies) Dialog?
+        // Then ressurect
+        // Demon mode
+        // Sqirtle spin
+        // Slams
+    }
+}
