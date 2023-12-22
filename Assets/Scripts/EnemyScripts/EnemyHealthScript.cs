52fe540f4693a8a73e685d74c05669a5cfce67cc
Arix581 <espenben000@alpinesd.org>
2023-12-21 18:57:08 -0700

    Started work on turtle attacks.

--- Assets/Scripts/EnemyScripts/EnemyHealthScript.cs
+++ Assets/Scripts/EnemyScripts/EnemyHealthScript.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyHealthScript : HealthScript
 {
     //public FloatObject points;
     //public float pointsOnDeath;
     public bool delOnDie = true;
+    public UnityEvent onDie;
     public override void Die()
     {
         base.Die();
         //points.value += pointsOnDeath;
+        onDie.Invoke();
         if (delOnDie)
         {
             Destroy(gameObject);
